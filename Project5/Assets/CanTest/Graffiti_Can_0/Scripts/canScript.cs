f43c0aeb716a79dbb31565a34896a1f1e2f15625
DiogoSilv4 <73476561+DiogoSilv4@users.noreply.github.com>
2021-11-03 08:49:37 +0000

    k

---
+++ Project5/Assets/CanTest/Graffiti_Can_0/Scripts/canScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canScript : MonoBehaviour
+{
+    [SerializeField] private GameObject CanSpot;
+    public Color CanColor = Color.green;
+    [Range(0.01f, 90f)] public float canTap = 15.0f;
+    public float range = 1.0f;
+    [SerializeField] private Transform controller;
+    private Outline outline = null;
+
+    [SerializeField] private float ControllerDistance = 1.0f;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(controller.position, this.transform.position);
+
+        if (dist <= ControllerDistance && outline == null)
+        {
+
+            outline = gameObject.AddComponent<Outline>();
+
+            outline.OutlineMode = Outline.Mode.OutlineVisible;
+            outline.OutlineColor = CanColor;
+            outline.OutlineWidth = 10f;
+
+        }
+        else if (dist > ControllerDistance)
+        {
+
+            Destroy(outline);
+        }
+
+    }
+}
